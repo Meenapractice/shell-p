47cee93b72f24159ff96650e9f5730dd4daf78c1
meena_test <abc@gmail.com>
2026-03-23 05:03:53 +0530

    added script

--- 24-while-loop.sh
+++ 24-while-loop.sh
@@ -2,8 +2,12 @@
 
 count=1
 
-while [ $count -le 5 ]
-do
-  echo "Count is: $count"
-  ((count++))
-done
+# while [ $count -le 5 ]
+# do
+#   echo "Count is: $count"
+#   ((count++))
+# done
+
+while IFS= read -r line
+echo $line
+do < ./21-script-1.sh
