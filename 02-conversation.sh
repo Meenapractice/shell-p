b4b6481e3b4efc6aa1156c667195a63b928fb39c
meena_test <abc@gmail.com>
2026-03-22 05:32:43 +0530

    added script

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hi Suresh, How are you?"
+echo "Suresh: Hi Ramesh. i am fine"
+echo "Ramesh: what are you learning today Suresh"
+echo "Suresh: Ramesh i am learning shell scripting"
