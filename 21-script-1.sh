e98ba4b8b2beace4caf4ab69bdf0910f11b17dd0
meena_test <abc@gmail.com>
2026-03-22 19:39:10 +0530

    added script

---
+++ 21-script-1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COUNTRY=INDIA
+
+echo "Country is $COUNTRY"
+echo "PID of SCRIPT-1 is $$"
+
+sh 22-script-2.sh
---
+++ 22-script-2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Country is $COUNTRY"
+echo "PID of script-2 is $$"
