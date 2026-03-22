b05ca41fca1872ef5fdb2a65b842d709bae84172
meena_test <abc@gmail.com>
2026-03-22 06:28:59 +0530

    added script

--- 12-logs.sh
+++ 12-logs.sh
@@ -1,6 +1,6 @@
 USER_ID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script/"
-LOGS_FILE="$LOGS_FOLDER/$0.sh"
+LOGS_FILE="$LOGS_FOLDER/$0"
 
 if [ $USER_ID -ne 0 ]; then
    echo "Please run the script with root user"
