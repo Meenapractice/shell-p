a4f6fa8485d5e0b50242e163d2604149466e2070
meena_test <abc@gmail.com>
2026-03-23 05:14:53 +0530

    added script

---
+++ 23-delete-old-logs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LOGS_FOLDER=/home/ec2-user/repos/app-logs/
+LOGS_FILE="$LOGS_FOLDER/$(basename $0).log"
+
+if [ ! -d $LOGS_FOLDER ]; then
+    echo "Folder doesnot exist"
+    exit 1
+fi
+
+FILES_TO_DELETE=$(find . -iname "*.log" -type f -mtime +14)
+
