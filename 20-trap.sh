27b6f1d74d15e751740f357913ae45de842df410
meena_test <abc@gmail.com>
2026-03-22 07:51:33 +0530

    added script

---
+++ 20-trap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+trap 'echo "The error occured as $LINENO : $BASH_COMMAND"' ERR
+USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script/"
+LOGS_FILE="$LOGS_FOLDER/$0"
+
+mkdir -p $LOGS_FOLDER
+
+if [ $? -ne 0 ]; then
+  echo "Please run the script with root access" 
+  exit 1
+fi
+
+for package in $@
+do 
+  dnf install $package
+done
