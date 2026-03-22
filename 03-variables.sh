a00d54e9cbd6651635cdf76c2938eb73167fdcff
meena_test <abc@gmail.com>
2026-03-22 05:36:23 +0530

    added script

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hi $PERSON2..How are you?"
+echo "$PERSON2: I am fine $PERSON1"
+echo "$PERSON1: What are you learning $PERSON2"
+echo "$PERSON2: $PERSON1 i am learning shell scripting"
