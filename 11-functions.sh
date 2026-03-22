56f40d525aa4fbc22c173cfbd1b92a5204d9af67
meena_test <abc@gmail.com>
2026-03-22 06:17:18 +0530

    added script

---
+++ 11-functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+   echo "Please run the script with root access"
+   exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+      echo "$2.... FAILURE"
+      exit 1
+    else
+      echo "$2...SUCCESS"
+}
+
+dnf install nginx -y
+VALIDATE $? "Nginx installation"
+
+dnf install mysql -y
+VALIDATE $? "Mysql installation"
+
+dnf install nodejs -y
+VALIDATE $? "Nodejs installation"
