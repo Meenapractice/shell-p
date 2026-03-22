d4229f9a7e0b218bf2452478aac4559d0f89be57
meena_test <abc@gmail.com>
2026-03-22 06:08:03 +0530

    added script

---
+++ 10-loops.sh
@@ -0,0 +1,13 @@
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0]; then
+   echo "Please run the script as root user"
+   exit 1
+fi
+
+dnf install nginx
+if [ $? -ne 0 ]; then
+   echo "Nginx installation...FAILURE"
+else
+   echo "Nginx installation..SUCCESS"
+fi
