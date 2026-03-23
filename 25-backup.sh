120e9dd55b03bf2c036d53875c6deb0da0425a7d
meena_test <abc@gmail.com>
2026-03-23 07:08:19 +0530

    added script

--- 25-backup.sh
+++ 25-backup.sh
@@ -40,7 +40,33 @@ if [ ! -d $DEST_DIR ]; then
    exit 1
 fi
 
-FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 
 echo $FILES
 
+log "Backup started"
+log "SOurce file exists"
+log "Destination file exists"
+
+if [ -z "$FILES" ]; then
+ echo "No files to archive"
+else
+  echo "Files found for archieving"
+  TIMESTAMP=$(date +%F-%H-%M-%S)
+  ZIP_FILE_NAMES="$DEST_DIR/app-logs-$TIMESTAMP.tar.gz"
+  echo "Archiving $ZIP_FILE_NAMES"
+  tar -zcvf $ZIP_FILE_NAMES $(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+
+    if [ -f "$ZIP_FILE_NAMES" ]; then
+      echo "Archive is SUCCESS"
+       while IFS= read -r filepath; do
+        # Process each line here
+        log "Deleting file: $filepath"
+        rm -f $filepath
+        log "Deleted file: $filepath"
+        done <<< $FILES
+    else
+        log "Archeival is ... $R FAILURE $N"
+        exit 1
+    fi
+fi
